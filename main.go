5d7bb390f1a7422b6e789ae528c135a4187809cc
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2026-06-24 20:12:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func StartHysteria2(configPath *C.char) C.int {
 	// 将 Node.js 传过来的 C 字符串转换为 Go 字符串
 	path := C.GoString(configPath)
 	
-	// 模拟命令行参数：相当于执行 `./hysteria server -c config.yaml`
+	// 模拟命令行参数：执行 `./hysteria server -c config.yaml`
 	os.Args = []string{"hysteria", "server", "-c", path}
 	
 	// 使用 Goroutine 在后台异步启动 Hysteria，防止阻塞 Node.js 主线程
@@ -24,5 +24,5 @@ func StartHysteria2(configPath *C.char) C.int {
 	return 0
 }
 
-// 编译为 c-shared 模式时，必须有一个空的 main 函数
+// 编译为 c-shared 模式，必须有一个空的 main 函数
 func main() {}
